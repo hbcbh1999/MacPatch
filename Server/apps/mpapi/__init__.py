db6bd2d368f81d59d7dabff514f482fc8d0612c5
Charlie Heizer <heizer1@llnl.gov>
2017-01-04 09:37:36 -0800

    Fix logging level issue

--- Server/apps/mpapi/__init__.py
+++ Server/apps/mpapi/__init__.py
@@ -15,15 +15,15 @@ def create_app(config_object=DefaultConfig):
 
     app = Flask(__name__)
 
-	# Add teardown_request to the app, here we ensure the db session is removed after the
-	# request has been completed in any way.
-	# db.session.remove will call rollback explicitly if needed
+    # Add teardown_request to the app, here we ensure the db session is removed after the
+    # request has been completed in any way.
+    # db.session.remove will call rollback explicitly if needed
     @app.teardown_request
     def shutdown_session(exception):
-		if not exception:
-			db.session.commit()
+        if not exception:
+            db.session.commit()
 
-		db.session.remove()
+        db.session.remove()
 
     app.config.from_object(config_object)
     app.config.from_pyfile('../config.cfg', silent=True)
@@ -34,27 +34,25 @@ def create_app(config_object=DefaultConfig):
     app.config['SQLALCHEMY_DATABASE_URI'] = _uri
 
     # Configure logging
-    # handler = logging.FileHandler(app.config['LOGGING_LOCATION'])
-    # handler = logging.handlers.RotatingFileHandler(app.config['LOGGING_LOCATION'], maxBytes=20, backupCount=30)
     log_file = app.config['LOGGING_LOCATION'] + "/mpwsapi.log"
     if not os.path.exists(app.config['LOGGING_LOCATION']):
-		os.makedirs(app.config['LOGGING_LOCATION'])
-		subprocess.call(['chmod', '2775', app.config['LOGGING_LOCATION']])
+        os.makedirs(app.config['LOGGING_LOCATION'])
+        subprocess.call(['chmod', '2775', app.config['LOGGING_LOCATION']])
 
     handler = logging.handlers.TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=30)
-	
-	if app.config['LOGGING_LEVEL'].lower() == 'info':
-		handler.setLevel(logging.INFO)
-	elif app.config['LOGGING_LEVEL'].lower() == 'debug':
-		handler.setLevel(logging.DEBUG)
-	elif app.config['LOGGING_LEVEL'].lower() == 'warning':
-		handler.setLevel(logging.WARNING)
-	elif app.config['LOGGING_LEVEL'].lower() == 'error':
-		handler.setLevel(logging.ERROR)
-	elif app.config['LOGGING_LEVEL'].lower() == 'critical':
-		handler.setLevel(logging.CRITICAL)
-	else:
-		handler.setLevel(logging.INFO)
+
+    if app.config['LOGGING_LEVEL'].lower() == 'info':
+        app.logger.setLevel(logging.INFO)
+    elif app.config['LOGGING_LEVEL'].lower() == 'debug':
+        app.logger.setLevel(logging.DEBUG)
+    elif app.config['LOGGING_LEVEL'].lower() == 'warning':
+        app.logger.setLevel(logging.WARNING)
+    elif app.config['LOGGING_LEVEL'].lower() == 'error':
+        app.logger.setLevel(logging.ERROR)
+    elif app.config['LOGGING_LEVEL'].lower() == 'critical':
+        app.logger.setLevel(logging.CRITICAL)
+    else:
+        app.logger.setLevel(logging.INFO)
     
     formatter = logging.Formatter(app.config['LOGGING_FORMAT'])
     handler.setFormatter(formatter)
@@ -67,67 +65,67 @@ def create_app(config_object=DefaultConfig):
 
 def read_siteconfig_server_data(app):
 
-	data = {}
-	if os.path.exists(app.config['SITECONFIG_FILE'].strip()):
-		try:
-			with open(app.config['SITECONFIG_FILE'].strip()) as data_file:
-				data = json.load(data_file)
+    data = {}
+    if os.path.exists(app.config['SITECONFIG_FILE'].strip()):
+        try:
+            with open(app.config['SITECONFIG_FILE'].strip()) as data_file:
+                data = json.load(data_file)
 
-		except OSError:
-			print('Well darn.')
-			return
+        except OSError:
+            print('Well darn.')
+            return
 
-	else:
-		print("Error, could not open file " + app.config['SITECONFIG_FILE'].strip())
-		return
+    else:
+        print("Error, could not open file " + app.config['SITECONFIG_FILE'].strip())
+        return
 
-	if "settings" in data:
-		app.config['MP_SETTINGS'] = data['settings']
-		return
+    if "settings" in data:
+        app.config['MP_SETTINGS'] = data['settings']
+        return
 
 def register_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
 
 def register_blueprints(app):
-	from .agent import agent as bp_agent
-	app.register_blueprint(bp_agent, url_prefix=app.config['URL_PREFIX'])
+    from .agent import agent as bp_agent
+    app.register_blueprint(bp_agent, url_prefix=app.config['URL_PREFIX'])
 
-	from .antivirus import antivirus as bp_antivirus
-	app.register_blueprint(bp_antivirus, url_prefix=app.config['URL_PREFIX'])
+    from .antivirus import antivirus as bp_antivirus
+    app.register_blueprint(bp_antivirus, url_prefix=app.config['URL_PREFIX'])
 
-	from .auth import auth as bp_auth
-	app.register_blueprint(bp_auth, url_prefix=app.config['URL_PREFIX'])
+    from .auth import auth as bp_auth
+    app.register_blueprint(bp_auth, url_prefix=app.config['URL_PREFIX'])
 
-	from .autopkg import autopkg as bp_autopkg
-	app.register_blueprint(bp_autopkg, url_prefix=app.config['URL_PREFIX'])
+    from .autopkg import autopkg as bp_autopkg
+    app.register_blueprint(bp_autopkg, url_prefix=app.config['URL_PREFIX'])
 
-	from .checkin import checkin as bp_checkin
-	app.register_blueprint(bp_checkin, url_prefix=app.config['URL_PREFIX'])
+    from .checkin import checkin as bp_checkin
+    app.register_blueprint(bp_checkin, url_prefix=app.config['URL_PREFIX'])
 
-	from .inventory import inventory as bp_inventory
-	app.register_blueprint(bp_inventory, url_prefix=app.config['URL_PREFIX'])
+    from .inventory import inventory as bp_inventory
+    app.register_blueprint(bp_inventory, url_prefix=app.config['URL_PREFIX'])
 
-	from .mac_profiles import mac_profiles as bp_mac_profiles
-	app.register_blueprint(bp_mac_profiles, url_prefix=app.config['URL_PREFIX'])
+    from .mac_profiles import mac_profiles as bp_mac_profiles
+    app.register_blueprint(bp_mac_profiles, url_prefix=app.config['URL_PREFIX'])
 
-	from .patches import patches as bp_patches
-	app.register_blueprint(bp_patches, url_prefix=app.config['URL_PREFIX'])
+    from .patches import patches as bp_patches
+    app.register_blueprint(bp_patches, url_prefix=app.config['URL_PREFIX'])
 
-	from .provisioning import provisioning as bp_provisioning
-	app.register_blueprint(bp_provisioning, url_prefix=app.config['URL_PREFIX'])
+    from .provisioning import provisioning as bp_provisioning
+    app.register_blueprint(bp_provisioning, url_prefix=app.config['URL_PREFIX'])
 
-	from .register import register as bp_register
-	app.register_blueprint(bp_register, url_prefix=app.config['URL_PREFIX'])
+    from .register import register as bp_register
+    app.register_blueprint(bp_register, url_prefix=app.config['URL_PREFIX'])
 
-	from .servers import servers as bp_servers
-	app.register_blueprint(bp_servers, url_prefix=app.config['URL_PREFIX'])
+    from .servers import servers as bp_servers
+    app.register_blueprint(bp_servers, url_prefix=app.config['URL_PREFIX'])
 
-	from .software import software as bp_software
-	app.register_blueprint(bp_software, url_prefix=app.config['URL_PREFIX'])
+    from .software import software as bp_software
+    app.register_blueprint(bp_software, url_prefix=app.config['URL_PREFIX'])
 
-	from .srv_utils import srv as bp_srv_utils
-	app.register_blueprint(bp_srv_utils, url_prefix=app.config['URL_PREFIX'])
+    from .srv_utils import srv as bp_srv_utils
+    app.register_blueprint(bp_srv_utils, url_prefix=app.config['URL_PREFIX'])
 
-	from .status import status as bp_status
-	app.register_blueprint(bp_status, url_prefix=app.config['URL_PREFIX'])
+    from .status import status as bp_status
+    app.register_blueprint(bp_status, url_prefix=app.config['URL_PREFIX'])
